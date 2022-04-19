1ca84be33cc608a10b3bb8d5a72a96ba890302b9
Lindoelson Lopes <lindoelson_lopes@hotmail.com>
2022-04-19 09:04:38 -0300

    Requisito 8 - Implementar a lógica para que a tabela seja alimentada pelo estado da aplicação

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
       <div>
         <table>
@@ -18,12 +20,48 @@ class Table extends Component {
               <th>Moeda de conversão</th>
               <th>Editar/Excluir</th>
             </tr>
+            { expenses.map(({
+              description,
+              tag,
+              method,
+              value,
+              currency,
+              exchangeRates,
+              id,
+            }) => {
+              const { name, ask } = exchangeRates[currency];
+              return (
+                <tr key={ id }>
+                  <td>{description}</td>
+                  <td>{tag}</td>
+                  <td>{method}</td>
+                  <td>{Number(value).toFixed(2).toString()}</td>
+                  <td>{currency}</td>
+                  <td>{name}</td>
+                  <td>{Number(ask).toFixed(2).toString()}</td>
+                  <td>{Number(value * ask).toFixed(2).toString()}</td>
+                  <td>Real</td>
+                </tr>
+              );
+            })}
           </thead>
         </table>
       </div>
     );
   }
 }
+
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number,
+    value: PropTypes.number,
+    description: PropTypes.string,
+    currency: PropTypes.string,
+    method: PropTypes.string,
+    tag: PropTypes.string,
+    exchangeRates: PropTypes.objectOf(PropTypes.any),
+  })).isRequired,
+};
 
 const mapStateToProps = (state) => ({
   expenses: state.wallet.expenses,
